995cf2c125eb9e1141111cf631f4ab4050a1909a
Christine <2739361+christine-aqui@users.noreply.github.com>
2019-11-25 23:46:34 -0500

    part2 - added routes for transfer and payments following task

--- src/app.js
+++ src/app.js
@@ -1,59 +1,38 @@
-// quire Built-in Libraries
 const fs = require('fs');
 const path = require('path');
 
-// quire Built-in Libraries
 const express = require('express');
+const app = new express();
 
-const app = express();
+const {
+  accounts,
+  users,
+  writeJSON
+} = require('./data.js');
 
-// Configure the View Directory and Engine
-app.set('views', path.join(__dirname, 'views'));
+const accountRoutes = require('./routes/accounts.js')
+const servicesRoutes = require('./routes/services.js')
+
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-// Configure the Static Directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({
+  extended: true
+}));
 
-//
-const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+app.get('/', (req, res) => res.render('index', {
+  title: 'Account Summary',
+  accounts: accounts
+}));
 
-const accounts = JSON.parse(accountData);
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+app.get('/profile', (req, res) => res.render('profile', {
+  user: users[0]
+}));
 
-const users = JSON.parse(userData);
-
-
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Account Summary',
-    accounts
-  })
+app.listen(3000, () => {
+  console.log('PS Project Running on port 3000!')
 });
-
-app.get('/savings', (req, res) => {
-  res.render('account', {
-    account: accounts.savings
-  });
-});
-
-app.get('/checking', (req, res) => {
-  res.render('account', {
-    account: accounts.checking
-  });
-});
-
-app.get('/credit', (req, res) => {
-  res.render('account', {
-    account: accounts.credit
-  });
-});
-
-app.get('/profile', (req, res) => {
-  res.render('profile', {
-    user: users[0]
-  });
-});
-
-
-app.listen(3000, () => console.log('Connected on Port:3000'));
